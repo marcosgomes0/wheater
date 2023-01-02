d99fa000558158e9333e22c3a43bd9e6fb089173
Marcos Gomes <marcosmuriloribeirogomes@gmail.com>
2023-01-02 18:56:48 -0300

    ajuste de dist

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -46,7 +46,7 @@ function onChange(e: React.SyntheticEvent<EventTarget>){
   }
 }
 
-
+console.log(tempo)
   return (
     <div className="home">
       <form className="form" onSubmit={handleSubmit}>
